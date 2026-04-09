d00299f12c10a003a68b5b81e8b8dc7ea7a77e80
dpkretzer <dpkretzer@gmail.com>
2026-04-08 18:31:36 -0700

    Update static file serving and port configuration

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ JavaScript
 const express = require('express');
 const app = express();
 
-app.use(express.static('.'));
+app.use(express.static(__dirname));
 
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log('Server running on port ' + PORT);
 });
