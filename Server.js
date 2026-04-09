4e2c7af09265e007edf997ff57c9903399f826dc
dpkretzer <dpkretzer@gmail.com>
2026-04-08 18:28:07 -0700

    Delete Server.js

--- Server.js
+++
@@ -1,9 +0,0 @@
-JavaScript
-const express = require('express');
-const app = express();
-
-app.use(express.static('.'));
-
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
-});
